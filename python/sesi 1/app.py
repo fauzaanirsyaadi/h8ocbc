4f202937c277185fae5dc15f634726130fe96264
fauzaanirsyaadi <fauzaan.irsyaadi@gmail.com>
2021-11-08 14:02:28 +0700

    add code

---
+++ python/sesi 1/app.py
@@ -0,0 +1,33 @@
+name = "hactiv 8"
+age = 25
+check = True
+
+# print variable
+print(name, age, check)
+print(4 % 16)
+
+s = 'FOO'
+t = 'bar'
+print(s + t)
+print(s*4)
+print(s.capitalize())
+print(s.lower())
+print(s.swapcase())
+print(s.upper())
+print('case fold')
+print(s.casefold())
+print()
+# build list and print variable
+lists = ['foo', 'bar', 'baz', 'lux', 'zoo']
+print(lists)
+print(lists[0])
+# list ordering
+print(sorted(lists))
+print(sorted(lists, reverse=True))
+
+# print(lists(0))
+
+# build tupple assignment   and print variable
+tup = ('foo', 'bar', 'baz', 'lux', 'zoo')
+print(tup)
+print(tup[0])
---
+++ python/sesi 2/app.py
@@ -0,0 +1,22 @@
+# me : hallo, copilot
+# copilot : hallo, me
+
+# the break and continue statements
+# break : terminates the loop
+# continue : skips the current iteration
+n = 5
+while n > 0:
+    n -= 1
+    if n == 2:
+        # break
+        continue
+    print(n)
+print("loop ended")
+
+# if else example
+# if condition:
+#   statement(s)
+# elif condition:
+#   statement(s)
+# else:
+#   statement(s)

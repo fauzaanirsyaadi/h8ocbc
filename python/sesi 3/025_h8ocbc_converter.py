cf6a3f37f84c3311b6b8e3da2b187ae8f54f01b6
fauzaanirsyaadi <fauzaan.irsyaadi@gmail.com>
2021-11-09 16:31:22 +0700

    simplify

--- python/sesi 3/025_h8ocbc_converter.py
+++ python/sesi 3/025_h8ocbc_converter.py
@@ -1,4 +1,3 @@
-
 # function to convert the celcius to kelvin
 def convert(celsius):
     kelvin = celsius + 273.15
@@ -10,13 +9,15 @@ def convert2(kelvin):
     celsius = kelvin - 273.15
     return celsius
 
+
 # function to convert the kelvin to fahrenheit and give arguments to check the input
 def convert3(kelvin):
     if kelvin < -273.15:
         print("The value is not possible to convert")
     else:
-        fahrenheit = (kelvin - 273.15) * 9/5 + 32
+        fahrenheit = (kelvin - 273.15) * 9 / 5 + 32
         return fahrenheit
+
 
 # function to convert the celsius to fahrenheit and give arguments to check the input
 def convert4(celsius):
@@ -27,29 +28,31 @@ def convert4(celsius):
     elif celsius == 0:
         print("The value is not possible to convert")
     # check input is not fahrenheit
-    elif celsius > -273.15 and celsius < -459.67:
+    elif -273.15 < celsius < -459.67:
         print("The value is not possible to convert")
     else:
-        fahrenheit = (celsius - 273.15) * 9/5 + 32
+        fahrenheit = (celsius - 273.15) * 9 / 5 + 32
         return fahrenheit
+
 
 # function to convert the fahrenheit to kelvin and give documentation to check the anf arguments to check the output
 def convert5(fahrenheit):
     if fahrenheit < -459.67:
         print("The value is not possible to convert")
     else:
-        kelvin = (fahrenheit - 32) * 5/9 + 273.15
+        kelvin = (fahrenheit - 32) * 5 / 9 + 273.15
         if kelvin < -273.15:
             print("The value is not possible to convert")
         else:
             return kelvin
+
 
 # function to convert the fahrenheit to Celsius and give arguments to check the output is celsius
 def convert6(fahrenheit):
     if fahrenheit < -459.67:
         print("The value is not possible to convert")
     else:
-        celsius = (fahrenheit - 32) * 5/9
+        celsius = (fahrenheit - 32) * 5 / 9
         # check the output is not kelvin
         if celsius < -273.15:
             print("The value is not possible to convert")
